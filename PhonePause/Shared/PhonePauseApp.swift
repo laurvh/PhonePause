5567f85d1a8af7d090b29fe8a1fe18eb62971453
laurvh <123323716+laurvh@users.noreply.github.com>
2023-02-17 16:37:15 -0800

    import files

---
+++ PhonePause/Shared/PhonePauseApp.swift
@@ -0,0 +1,16 @@
+//
+//  PhonePauseApp.swift
+//  Shared
+//
+//
+
+import SwiftUI
+
+@main
+struct PhonePauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
