a7ea2d0afc4d28bc65ddf428a53c4731f6ea7eb7
laurvh <123323716+laurvh@users.noreply.github.com>
2023-03-17 20:00:21 -0700

    Uploading

---
+++ PhonePause/PhonePause/PhonePauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhonePauseApp.swift
+//  PhonePause
+//
+//  Created by Lauren Van Horn on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PhonePauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
